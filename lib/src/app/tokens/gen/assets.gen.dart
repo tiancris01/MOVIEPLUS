de07b99d593adc7f2e37b412cbb1753d5c051a27
Daniel osorio <tiancris01@gmail.com>
2024-09-06 19:25:06 -0500

    feat: Add generated code for assets and colors

---
+++ lib/src/app/tokens/gen/assets.gen.dart
@@ -0,0 +1,33 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsEnvGen {
+  const $AssetsEnvGen();
+
+  /// File path: assets/env/.env.dev
+  String get aEnvDev => 'assets/env/.env.dev';
+
+  /// File path: assets/env/.env.prod
+  String get aEnvProd => 'assets/env/.env.prod';
+
+  /// File path: assets/env/.env.qa
+  String get aEnvQa => 'assets/env/.env.qa';
+
+  /// File path: assets/env/.env.template
+  String get aEnvTemplate => 'assets/env/.env.template';
+
+  /// List of all assets
+  List<String> get values => [aEnvDev, aEnvProd, aEnvQa, aEnvTemplate];
+}
+
+class AssetsToken {
+  AssetsToken._();
+
+  static const $AssetsEnvGen env = $AssetsEnvGen();
+}
