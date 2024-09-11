1beccb7919dd11767ae01002369f324ed58bca12
Daniel osorio <tiancris01@gmail.com>
2024-09-11 08:34:46 -0500

    Refactor: Update main.dart and main_dev.dart for Firebase initialization and environment configuration

---
+++ lib/src/app/config/base_config/base_app_config.dart
@@ -0,0 +1,5 @@
+abstract class BaseAppConfig {
+  String get environment;
+  String get movieDbApiKey;
+  bool get bannerVisibility;
+}
---
+++ lib/src/app/config/base_config/qa_config.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movieplus/src/app/config/base_config/base_config_exports.dart';
+
+class QaConfig extends BaseAppConfig {
+  @override
+  bool get bannerVisibility =>
+      dotenv.env['BANNER_VISIBILITY']?.contains("true") ?? false;
+
+  @override
+  String get environment => dotenv.env['ENVIRONMENT'] ?? '';
+
+  @override
+  String get movieDbApiKey => dotenv.env['MOVIEDB_API_KEY'] ?? '';
+}
