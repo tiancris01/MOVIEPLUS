d5bf105a21d228b34bde844800d958282b31c707
Daniel osorio <tiancris01@gmail.com>
2024-09-12 16:05:11 -0500

    Add AppResponse class for handling API response data

---
+++ lib/src/shared/data/models/response.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'response.freezed.dart';
+
+@freezed
+class AppResponse with _$AppResponse {
+  const AppResponse._();
+  const factory AppResponse({
+    required dynamic data,
+    required int statusCode,
+    required String statusMessage,
+  }) = _AppResponse;
+}
